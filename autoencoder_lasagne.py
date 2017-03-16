77ecabab16e85b7c3c11300e33efa9ce1d85a047
Lucas Caccia <Lucas Caccia>
2017-03-16 14:14:00 -0400

    back to unconditional GAN

--- autoencoder_lasagne.py
+++ autoencoder_lasagne.py
@@ -24,58 +24,67 @@ from model import *
 # theano.config.openmp = True
 
 # Hyperparameters :   
-batch_size = 50
+batch_size = 64
 num_epochs = 50
 n_critic = 5
 
-initial_lambda_sqr = 0.95
-initial_eta = 0.0003
+initial_lambda_sqr = 0.
+initial_eta = 2e-3
 full_img = False
 
 
-#%%
+
 model = Model(version=2, batch_size=batch_size, full_img=full_img)
 auto_encoder = model.model[-1]
 critic = model.critic[-1]
 
+
+'''
 # Instantiate a symbolic noise generator to use for training
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 srng = RandomStreams(seed=np.random.randint(2147462579, size=6))
 noise = srng.normal((batch_size, 3, 64, 64), avg=0, std=1)
-
+'''
 #%%
 print 'param setup'
 ae_output = lasagne.layers.get_output(auto_encoder)
+ae_params = lasagne.layers.get_all_params(auto_encoder, trainable=True)
+
+eta = theano.shared(lasagne.utils.floatX(initial_eta))
+lambda_sqr = theano.shared(lasagne.utils.floatX(initial_lambda_sqr))
+lambda_cr = theano.shared(lasagne.utils.floatX(1 - initial_lambda_sqr))
+'''
+ae_sqr_loss = lambda_sqr * (
+        1 * lasagne.objectives.squared_error(ae_output, model.output_).mean() +
+        15 * (contour_delta_tensor(model.input_, ae_output) ** 2).mean() )
+'''
+
 critic_output = lasagne.layers.get_output(critic)
 
 # Create expression for passing real data through the critic
 real_out = lasagne.layers.get_output(critic, inputs=model.input_c)
 fake_out = lasagne.layers.get_output(critic,
                             inputs=ae_output)
-
-# hidden layers for feature matching
+'''# hidden layers for feature matching
 hid_real = ll.get_output(model.critic[-3], inputs=model.input_c, deterministic=False)
 hid_fake = ll.get_output(model.critic[-3], inputs=ae_output, deterministic=False)
 m1 = T.mean(hid_real,axis=0)
 m2 = T.mean(hid_fake,axis=0)
-loss_gen_fm = T.mean(abs(m1-m2)) # feature matching los
-
+loss_gen_fm = T.mean(abs(m1-m2)) # feature matching loss
+'''
 # Create update expressions for training
-ae_params = lasagne.layers.get_all_params(auto_encoder, trainable=True)
 critic_params = lasagne.layers.get_all_params(critic, trainable=True)
-eta = theano.shared(lasagne.utils.floatX(initial_eta))
-lambda_sqr = theano.shared(lasagne.utils.floatX(initial_lambda_sqr))
-lambda_cr = theano.shared(lasagne.utils.floatX(1 - initial_lambda_sqr))
+#critic_loss = (lasagne.objectives.binary_crossentropy(real_out, 1)
+#        + lasagne.objectives.binary_crossentropy(fake_out, 0)).mean()
+critic_loss = 0.5 * T.mean( (real_out - 1) ** 2) + 0.5 * T.mean((fake_out) ** 2)
 
-ae_sqr_loss = lambda_sqr * (
-        1 * lasagne.objectives.squared_error(ae_output, model.output_).mean() + 
-        10 * contour_delta_tensor(model.input_, ae_output).mean() )
-ae_cr_loss = lambda_cr * (0.9* loss_gen_fm + 0.1 * lasagne.objectives.binary_crossentropy(fake_out, 0.9).mean())
+ae_cr_loss = lambda_cr * (T.mean( (fake_out -1) ** 2)) #(lasagne.objectives.binary_crossentropy(fake_out, 0.95).mean())
+#                             loss_gen_fm )
 
-ae_loss = ae_sqr_loss + ae_cr_loss
+#ae_cr_loss = lambda_cr * ((lasagne.objectives.binary_crossentropy(fake_out, 0.95).mean()))# + loss_gen_fm))
 
-critic_loss = (lasagne.objectives.binary_crossentropy(real_out, 1)
-        + lasagne.objectives.binary_crossentropy(fake_out, 0)).mean()
+ae_loss = ae_cr_loss # ae_sqr_loss
+
 
 #%%
 '''
@@ -83,27 +92,27 @@ here we calculate the gradient wrt to both adversarial and MSE loss for the
 autoencoder. This allows us to compare which loss is "pulling" the hardest
 '''
 print 'loss and function setup'
-ae_mse_grad = theano.grad(ae_sqr_loss, wrt=ae_params)
+
+#ae_mse_grad = theano.grad(ae_sqr_loss, wrt=ae_params)
+#ae_mse_grad_norm = sum(T.sum(T.square(grad)) for grad in ae_mse_grad) / len(ae_mse_grad)
 ae_cr_grad = theano.grad(ae_cr_loss, wrt=ae_params)
-ae_grads = [ x + y for (x,y) in zip(ae_mse_grad, ae_cr_grad)] 
+
+
+ae_grads = ae_cr_grad#[ x + y for (x,y) in zip(ae_mse_grad, ae_cr_grad)]
 
 critic_grads = theano.grad(critic_loss, wrt=critic_params)
-
-ae_mse_grad_norm = sum(T.sum(T.square(grad)) for grad in ae_mse_grad) / len(ae_mse_grad)
 ae_cr_grad_norm = sum(T.sum(T.square(grad)) for grad in ae_cr_grad) / len(ae_cr_grad)
-
 critic_grads_norm = sum(T.sum(T.square(grad)) for grad in critic_grads) / len(critic_grads)
 
 ae_updates= lasagne.updates.adam(
     ae_grads, ae_params, learning_rate=eta, beta1=0.5)
 
 critic_updates = lasagne.updates.adam(
-    critic_grads, critic_params, learning_rate=0.1*eta, beta1=0.5)
+    critic_grads, critic_params, learning_rate=0.08*eta, beta1=0.5)
 
-
-train_critic = theano.function(inputs=[model.input_, model.input_c], 
-                               outputs=[real_out, 
-                                        fake_out, 
+train_critic = theano.function(inputs=[model.input_c], #inputs=[model.input_, model.input_c],
+                               outputs=[#real_out,
+                                        #fake_out,
                                         (real_out > .5).mean(),
                                         (fake_out < .5).mean(),
                                         critic_loss,
@@ -112,18 +121,25 @@ train_critic = theano.function(inputs=[model.input_, model.input_c],
                                #givens={model.noise_var: noise},
                                name='train_critic')
 
-
-train_ae = theano.function(inputs=[model.input_, model.output_, model.input_c], 
+train_ae = theano.function(inputs=[],#model.input_, model.output_],#, model.input_c],
                            outputs=[(fake_out > .5).mean(), 
-                                    ae_mse_grad_norm,
+                                    #ae_mse_grad_norm,
                                     ae_cr_grad_norm,
                                     ae_loss, 
                                     ae_output], 
                            #givens={model.noise_var: noise},
                            updates = ae_updates,
                            name='train_ae')
+'''
+train_ae_mse = theano.function(inputs=[model.input_, model.output_],
+                               outputs=[ae_mse_grad_norm, 
+                                        ae_mse_loss, 
+                                        ae_output], 
+                               updates = lasagne.updates.adam(ae_mse_grad, ae_params, learning_rate=eta, beta1=0.5),
+                               name='train_ae_mse')
+'''
 
-test_ae = theano.function(inputs=[model.input_], 
+test_ae = theano.function(inputs=[],#model.input_],
                            #givens={model.noise_var: noise},
                            outputs=[ae_output], 
                            name='test_ae')
@@ -138,10 +154,10 @@ batches = iterate_minibatches(trainx, trainy, batch_size, shuffle=True,
 
 input_test, target_test = testx[:batch_size], testy[:batch_size]
 
-#%%
+
 num_batches = trainx.shape[0] / batch_size
 
-for epoch in range(0, 3000) : 
+for epoch in range(0, 50) :
     print epoch
     updates_gen = 0
     updates_critic = 0
@@ -154,19 +170,22 @@ for epoch in range(0, 3000) :
         # train autoencoder
         for _ in range(gen_iter):
             input, target = next(batches)
-            acc_ae, ae_mse_grad_norm, ae_cr_grad_norm, loss_ae, pred = train_ae(input, target, target)
-            ae_err += np.array([acc_ae, ae_mse_grad_norm, ae_cr_grad_norm, loss_ae])
+            acc_ae, ae_cr_grad_norm, loss_ae, pred = train_ae()#(input, target)#, target)
+            ae_err += np.array([acc_ae, ae_cr_grad_norm, loss_ae])
+            #ae_mse_grad_norm,  loss_ae, pred = train_ae(input, target)#, target)
+            #ae_err += np.array(train_ae())
             updates_gen += 1
 
         # train discriminator
         for _ in range(critic_iter):
-            input, target = next(batches)
-            real_out, fake_out, acc_real, acc_fake, loss_disc, cr_grad_norm = train_critic(input, target)
-            disc_err += np.array([acc_real, acc_fake, loss_disc, cr_grad_norm])       
+            #input, target = next(batches)
+            #real_out, fake_out, acc_real, acc_fake, loss_disc, cr_grad_norm = train_critic(input, target)
+            #disc_err += np.array([acc_real, acc_fake, loss_disc, cr_grad_norm])
+            disc_err += np.array([train_critic(target)])
             updates_critic += 1
 
     # test it out 
-    samples = test_ae(input_test)
+    samples = test_ae()#input_test)
     samples = samples[0]
     result = fill_middle_extra(input_test, samples)* 77.3 + 86.3
     result = result.transpose(0,2,3,1).astype('uint8')
@@ -174,9 +193,13 @@ for epoch in range(0, 3000) :
     
     saveImage(result, 'samples_', epoch)
     
-    if epoch % 5 == 4 : 
-        lambda_sqr.set_value(lasagne.utils.floatX(max(0.1, lambda_sqr.get_value() - 0.002)))
-        lambda_cr.set_value(lasagne.utils.floatX(min(1, lambda_cr.get_value() + 0.002)))
+    #if epoch % 3 >= 0 : 
+    #    lambda_sqr.set_value(lasagne.utils.floatX(max(0.1, lambda_sqr.get_value() - 0.001)))
+    #    lambda_cr.set_value(lasagne.utils.floatX(min(1, lambda_cr.get_value() + 0.001)))
+        
+    if epoch % 10 == 0 : 
+        np.savez('disc_params_5.npz', *[p.get_value() for p in critic_params])
+        np.savez('gen_params_5.npz', *[p.get_value() for p in ae_params])
     
     # Then we print the results for this epoch:
     print("  discriminator loss:\t\t{}".format(disc_err / updates_critic))
@@ -189,17 +212,8 @@ resultt = resultt.transpose(0,2,3,1).astype('uint8')
 Image.fromarray(resultt[0]).show()
 
 #%%
-f = file("data.bin","wb")
-np.save(f,trainx)
-np.save(f,trainy)
-np.save(f,trainz)
-np.save(f,testx)
-np.save(f,testy)
-np.save(f,testz)
-f.close()
-
-#%%
-
-
 np.savez('disc_params_5.npz', *[p.get_value() for p in critic_params])
 np.savez('gen_params_5.npz', *[p.get_value() for p in ae_params])
+#%%
+lambda_sqr.set_value(0.1)
+lambda_cr.set_value(0.9)
--- model.py
+++ model.py
@@ -13,13 +13,14 @@ import theano.tensor as T
 import lasagne
 import lasagne.layers as ll
 from lasagne.init import Normal
-from layers import * 
+from layers import *
+from theano.sandbox.rng_mrg import MRG_RandomStreams
 import nn
 
 '''
 class representing an actual model (lasagne/theano)
 '''
-class Model : 
+class Model :
     def __init__(self, name='lasagne_model', version=1, batch_size=64, full_img=False):
         self.name = name
         self.version = version
@@ -65,6 +66,7 @@ class Model :
     '''
     def build_model(self,version):
     
+        from lasagne.layers import TransposedConv2DLayer as Deconv2DLayer
         # first model is a regular auto encodeder with Fully Connected layer in 
         # the middle. See blog for more detailed architecture comments. 
         
@@ -75,26 +77,46 @@ class Model :
         
         if version == 2: 
             # encoder
+            '''
             gen_layers = [ll.InputLayer(shape=data_size, input_var=self.model_input)]
-            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 64, 5, 2, pad='same', nonlinearity=nn.lrelu)))
-            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 128, 3, 2, pad='same', nonlinearity=nn.lrelu)))
-            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 256, 3, 2, pad='same', nonlinearity=nn.lrelu)))
-            #gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 512, 3, 2, pad='same', nonlinearity=nn.lrelu)))
-            #gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 1024, 3, 2, pad='same', nonlinearity=nn.lrelu)))
+            # b_s x 3 x 64 x 64 --> b_s x 64 x 32 x 32
+            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 64, 4, 2, pad=1, nonlinearity=nn.lrelu)))
+            # b_s x 64 x 32 x 32 --> b_s x 64 x 16 x 16
+            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 64, 4, 2, pad=1, nonlinearity=nn.lrelu)))
+            # b_s x 64 x 16 x 16 --> b_s x 128 x 8 x 8
+            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 128, 4, 2, pad=1, nonlinearity=nn.lrelu)))
+            # b_s x 128 x 8 x 8 --> b_s x 256 x 4 x 4 
+            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 256, 4, 2, pad=1, nonlinearity=nn.lrelu)))
+            # b_s x 128 x 8 x 8 --> b_s x 512 x 2 x 2 
+            #gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 2048, 4, 2, pad='same', nonlinearity=nn.lrelu)))
+            # b_s x 256 x 4 x 4 --> b_s x 2048 x 1 x 1
             
-            # middle
-            #gen_layers.append((nn.batch_norm(ll.DenseLayer(gen_layers[-1], 300))))
-            gen_layers.append(ll.ReshapeLayer(nn.batch_norm(ll.DenseLayer(gen_layers[-1], 350)),  ([0], 14, 5, 5)))
-            #gen_layers.append(nn.batch_norm(channel_wise_fc_layer(gen_layers[-1])))
-            #gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 512, 3, 1, pad='same', nonlinearity=nn.lrelu)))
-            
+            gen_layers.append(nn.batch_norm(ll.Conv2DLayer(gen_layers[-1], 2048, 4, 4, pad=1, nonlinearity=nn.lrelu)))          
+            '''
+            noise_dim = (self.batch_size, 100)
+            theano_rng = MRG_RandomStreams(rng.randint(2 ** 15))
+            noise = theano_rng.uniform(size=noise_dim)
+            gen_layers = [ll.InputLayer(shape=noise_dim, input_var=noise)]
+            gen_layers.append(nn.batch_norm(
+                ll.DenseLayer(gen_layers[-1], num_units=5 * 5 * 512, W=Normal(0.05), nonlinearity=nn.relu), g=None))
+            gen_layers.append(ll.ReshapeLayer(gen_layers[-1], (self.batch_size, 512, 5, 5)))
+            gen_layers.append(nn.batch_norm(
+                nn.Deconv2DLayer(gen_layers[-1], (self.batch_size, 256, 10, 10), (5, 5), W=Normal(0.05),
+                                 nonlinearity=nn.relu), g=None))  # 4 -> 8
+            gen_layers.append(nn.batch_norm(
+                nn.Deconv2DLayer(gen_layers[-1], (self.batch_size, 128, 20, 20), (5, 5), W=Normal(0.05),
+                                 nonlinearity=nn.relu), g=None))  # 8 -> 16
+            gen_layers.append(nn.weight_norm(
+                nn.Deconv2DLayer(gen_layers[-1], (self.batch_size, 3, 40, 40), (5, 5), W=Normal(0.05),
+                                 nonlinearity=T.tanh), train_g=True, init_stdv=0.1))  # 16 -> 32
+            '''
             # decoder
             #gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (self.batch_size,1024,2,2), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 2 -> 4
-            #gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (self.batch_size,512,5,5), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 4 -> 8
-            gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (self.batch_size,256,10,10), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 4 -> 8
-            gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (self.batch_size,128,20,20), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 8 -> 16
+            gen_layers.append(nn.batch_norm(ll.Deconv2DLayer(gen_layers[-1], (self.batch_size,512,5,5), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 4 -> 8        
+            gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (self.batch_size,256,4,4), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 4 -> 8
+            gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (self.batch_size,128,8,8), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 8 -> 16
             gen_layers.append(nn.weight_norm(nn.Deconv2DLayer(gen_layers[-1], (self.batch_size,3,40,40), (5,5), W=Normal(0.05), nonlinearity=T.tanh), train_g=True, init_stdv=0.1)) # 16 -> 32
-            
+            '''
             for layer in gen_layers : 
                 print layer.output_shape
             print ''
@@ -152,50 +174,64 @@ class Model :
                                                        stride=(2,2),
                                                        crop=1)
             net['bn2_t'] = lasagne.layers.BatchNormLayer(net['c2_t'])
-            
-            net['output'] = lasagne.layers.TransposedConv2DLayer(net['bn2_t'],
-                                                       num_filters=3,
-                                                       filter_size=3,
-                                                       stride=(1,1),
-                                                       crop=1)
-            '''
-            net['output'] = lasagne.layers.TransposedConv2DLayer(net['bn1_t'],
-                                                       num_filters=3,
-                                                       filter_size=3,
-                                                       stride=(1,1),
-                                                       crop=1)
-            '''
+
+
             return net['output']
 
 
-    
-    
+
+
 
     '''
     construct critic to get adversarial loss gradient
+                                                       stride=(1,1),
+                                                       crop=1)
     '''
-    def build_critic(self, full_img, version=3):
+    def build_critic(self, full_img, version=4):
         #assert self.model != None
         
+        if version==4:
+            from lasagne.nonlinearities import sigmoid
+            disc_layers = [ll.InputLayer(shape=(None, 3, 40, 40), input_var=self.input_c)]
+            # b_s x 3 x 40 x 40 --> b_s x 32 x 20 x 20
+            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 32, (3,3), pad=1, stride=2,  W=Normal(0.03), nonlinearity=nn.lrelu)))#nn.weight_norm
+            #disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
+            # b_s x 32 x 20 x 20 --> b_s x 64 x 10 x 10 
+            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 64, (3,3), pad=1, stride=2,  W=Normal(0.03), nonlinearity=nn.lrelu)))#nn.weight_norm
+            #disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
+            # b_s x 64 x 10 x 10 --> b_s x 128 x 5 x 5
+            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 128, (3,3), pad=1, stride=2,  W=Normal(0.03),  nonlinearity=nn.lrelu)))#nn.weight_norm
+            # b_s x 128 x 5 x 5 --> b_s x 256 x 5 x 5
+            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 256, (3,3), pad=1, stride=2,  W=Normal(0.03), nonlinearity=nn.lrelu)))#nn.weight_norm
+            #disc_layers.append(ll.GlobalPoolLayer(disc_layers[-1]))
+            disc_layers.append(nn.MinibatchLayer(disc_layers[-1], num_kernels=40))
+            disc_layers.append((ll.DenseLayer(disc_layers[-1], num_units=1, W=Normal(0.03), nonlinearity=None)))#nn.weight_norm, train_g=True, init_stdv=0.1))#nn.weight_norm
+            
+            for layer in disc_layers : 
+                print layer.output_shape
+            print ''
+            
+            return disc_layers
+            
         # openAI improved techniques for GAN discriminator 
         if version==3:
             from lasagne.nonlinearities import sigmoid
             disc_layers = [ll.InputLayer(shape=(None, 3, 40, 40), input_var=self.input_c)]
             disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.2))
-            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 96, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
+           # disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 96, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
            # disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 96, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
-            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 96, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu)))
+            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 96, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu))) #nn.weight_norm
             disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
-            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 192, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
+            disc_layers.append((ll.Conv2DLayer(disc_layers[-1], 192, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu))) #nn.weight_norm
             #disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
-            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 192, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu)))
+            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 192, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu))) #nn.weight_norm
             disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
-            disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 192, (3,3), pad=0, W=Normal(0.05), nonlinearity=nn.lrelu)))
-            disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=192, W=Normal(0.05), nonlinearity=nn.lrelu)))
+            #disc_layers.append(nn.weight_norm(ll.Conv2DLayer(disc_layers[-1], 192, (3,3), pad=0, W=Normal(0.05), nonlinearity=nn.lrelu)))
+            disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=192, W=Normal(0.05), nonlinearity=nn.lrelu))) #
             #disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=192, W=Normal(0.05), nonlinearity=nn.lrelu)))
             disc_layers.append(ll.GlobalPoolLayer(disc_layers[-1]))
             disc_layers.append(nn.MinibatchLayer(disc_layers[-1], num_kernels=40))
-            disc_layers.append(nn.weight_norm(ll.DenseLayer(disc_layers[-1], num_units=1, W=Normal(0.05), nonlinearity=sigmoid), train_g=True, init_stdv=0.1))
+            #disc_layers.append(nn.weight_norm(ll.DenseLayer(disc_layers[-1], num_units=1, W=Normal(0.05), nonlinearity=sigmoid)) train_g=True, init_stdv=0.1) 
             
             for layer in disc_layers : 
                 print layer.output_shape
